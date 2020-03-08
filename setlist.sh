f00c7e38c7a344bbbc08819133feb60ba33ff595
kiperos <54328682+kiperos@users.noreply.github.com>
2020-03-08 23:09:26 +0300

    Update setlist.sh

--- setlist.sh
+++ setlist.sh
@@ -3,7 +3,7 @@
 apt update
 apt dist-upgrade -y
 apt install -y mc git wget apache2 php php-mcrypt php-curl
-cd /usr/local/bin && wget -O up https://raw.githubusercontent.com/kiperos/w/master/up.sh && chmod +x up
+cd /usr/local/bin && wget -O up https://raw.githubusercontent.com/kiperos/w/master/uplist.sh && chmod +x up
 echo "put list of domains in format: 
 prokladka donor
 prokladka2 donor2
