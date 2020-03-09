3f745b79179bef83276aa001785bd4b297f37578
kiperos <54328682+kiperos@users.noreply.github.com>
2020-03-09 22:13:40 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt dist-upgrade -y
-apt install -y mc git wget apache2 php php-mcrypt php-curl
+apt install -y mc git wget apache2 php php-mcrypt php-curl p7zip-full
 sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php/7.0/apache2/php.ini
 cd /usr/local/bin && wget -O up https://raw.githubusercontent.com/kiperos/w/master/up.sh && chmod +x up
 echo "put list of domains in format: 
